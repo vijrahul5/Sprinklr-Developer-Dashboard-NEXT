4b829a1356eff9bb2bfd65243c919e3cae5a4082
Rahul Vij <rahul.vij@sprinklr.com>
2021-07-28 18:37:44 +0530

    deploy

--- controller/gitlabNotificationController.js
+++ controller/gitlabNotificationController.js
@@ -1,20 +1,20 @@
-// const Pusher = require("pusher");
-// // require("dotenv").config();
+const Pusher = require("pusher");
+// require("dotenv").config();
 
-// const pusher = new Pusher({
-//     appId: process.env.APP_ID,
-//     key: process.env.KEY,
-//     secret: process.env.SECRET,
-//     cluster: "ap2",
-//     useTLS: true,
-// });
+const pusher = new Pusher({
+    appId: process.env.APP_ID,
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    cluster: "ap2",
+    useTLS: true,
+});
 
 function handleNotification(req, res) {
     res.sendStatus(200);
-    // console.log("req",req.body);
-    // pusher.trigger("gitlab-channel", `gitlabWebhook`, {
-    //     details: req.body,
-    // });
+    console.log("req",req.body);
+    pusher.trigger("gitlab-channel", `gitlabWebhook`, {
+        details: req.body,
+    });
 }
 
 module.exports.handleNotification = handleNotification;
